03936e88d86842bf690a8116d38ec52ac9c67f6d
NestorNavarro <nestor.navarroarizaga@gmail.com>
2023-01-04 17:33:38 -0600

    feat(example): new example textArea

--- examples/src/App.tsx
+++ examples/src/App.tsx
@@ -1,17 +1,20 @@
-import * as Yup                  from "yup";
-import { useForm, FormProvider } from "react-hook-form";
-import { yupResolver }           from "@hookform/resolvers/yup";
-import { RHFTextInput }          from "@inprodi/core";
-import { MantineProvider } from '@mantine/core';
+import * as Yup                      from "yup";
+import { useForm, FormProvider }     from "react-hook-form";
+import { yupResolver }               from "@hookform/resolvers/yup";
+import { RHFTextInput, RHFTextArea } from "../../packages/inprodi-core";
+import { MantineProvider }           from "@mantine/core";
 
 function App() {
 	const schema = Yup.object().shape({
 		myInpt : Yup.string()
 			.required("El campo es requerido"),
+		textArea : Yup.string()
+			.required("El campo es requerido"),
 	});
 	const methods = useForm({
 		defaultValues : {
-			myInpt : "",
+			myInpt   : "",
+			textArea : "",
 		},
 		resolver : yupResolver(schema),
 
@@ -21,18 +24,19 @@ function App() {
 		console.log(data);
 	};
 	return (
-		<MantineProvider theme={{ 
+		<MantineProvider theme={{
 			primaryShade : 6,
 			primaryColor : "primary",
-			colors : {
-				primary  : [ "#EAECF6", "#D4D9ED", "#AAB4DC", "#95A1D3", "#7F8ECA", "#5569B9", "#2A43A7", "#223686", "#192864", "#152254" ],
+			colors       : {
+				primary : [ "#EAECF6", "#D4D9ED", "#AAB4DC", "#95A1D3", "#7F8ECA", "#5569B9", "#2A43A7", "#223686", "#192864", "#152254" ],
 			},
 		}}>
-		<FormProvider {...methods}>
-			<form onSubmit={handleSubmit(onSubmit)}>
-				<RHFTextInput name="myInpt" label="Inpt" />
-			</form>
-		</FormProvider>
+			<FormProvider {...methods}>
+				<form onSubmit={handleSubmit(onSubmit)}>
+					<RHFTextInput name="myInpt" label="Inpt" />
+					<RHFTextArea name="textArea" label="TextArea"/>
+				</form>
+			</FormProvider>
 		</MantineProvider>
 	);
 }
