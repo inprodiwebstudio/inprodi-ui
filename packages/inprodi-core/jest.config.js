9e306dd6105f1985fc7f53c887da59d4dfe4f38d
NestorNavarro <nestor.navarroarizaga@gmail.com>
2023-01-10 14:02:57 -0600

    fix: transformIgnorePatterns added on jest.config and babel.config added

--- packages/inprodi-core/jest.config.js
+++ packages/inprodi-core/jest.config.js
@@ -1,4 +1,9 @@
 module.exports = {
 	verbose         : true,
 	testEnvironment : "jsdom",
+	transformIgnorePatterns : [
+		//https://jaketrent.com/post/jest-unexpected-token-typescript/
+		//https://jestjs.io/docs/configuration#transformignorepatterns-arraystring
+		"/node_modules/(?!(@inprodi/icons)/)",
+	],
 };
