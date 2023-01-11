8334de45a91ed0da849f28c01bd910e820e87677
NestorNavarro <nestor.navarroarizaga@gmail.com>
2023-01-11 16:42:56 -0600

    feat: icons example added

--- examples/src/App.tsx
+++ examples/src/App.tsx
@@ -1,7 +1,9 @@
-import * as Yup                           from "yup";
-import { useForm }                        from "react-hook-form";
-import { yupResolver }                    from "@hookform/resolvers/yup";
-import { Button, MantineProvider, Stack } from "@mantine/core";
+import * as Yup                                                           from "yup";
+import { useForm }                                                        from "react-hook-form";
+import { yupResolver }                                                    from "@hookform/resolvers/yup";
+import { ActionIcon, Button, Center, Group, Kbd, MantineProvider, Stack } from "@mantine/core";
+
+import { UserOutline, DiamondFilled, WordColor } from "../../packages/inprodi-icons/src";
 
 import {
 	FormProvider,
@@ -41,15 +43,33 @@ function App() {
 				primary : [ "#EAECF6", "#D4D9ED", "#AAB4DC", "#95A1D3", "#7F8ECA", "#5569B9", "#2A43A7", "#223686", "#192864", "#152254" ],
 			},
 		}}>
-			<FormProvider methods={methods} onSubmit={handleSubmit(onSubmit)}>
-				<Stack>
-					<RHFTextInput name="myInpt" label="Inpt" />
-					<RHFSwitch name="mySwitch" label="switch"/>
-					<RHFTextArea name="textArea" label="TextArea"/>
-					<RHFPasswordInput name="password" label="Password"/>
-					<Button type="submit" >Submit</Button>
-				</Stack>
-			</FormProvider>
+			<Stack>
+				<FormProvider methods={methods} onSubmit={handleSubmit(onSubmit)}>
+					<Stack p={25}>
+						<RHFTextInput name="myInpt" label="Inpt" />
+						<RHFSwitch name="mySwitch" label="switch"/>
+						<RHFTextArea name="textArea" label="TextArea"/>
+						<RHFPasswordInput name="password" label="Password"/>
+						<Button type="submit" >Submit</Button>
+					</Stack>
+				</FormProvider>
+				<Center>
+					<Kbd>Icons</Kbd>
+				</Center>
+				<Center>
+					<Group>
+						<ActionIcon color="primary" variant="subtle">
+							<UserOutline size="xl" />
+						</ActionIcon>
+						<ActionIcon color="primary" variant="subtle">
+							<DiamondFilled size="xl" />
+						</ActionIcon>
+						<ActionIcon color="primary" variant="subtle">
+							<WordColor fillOpacity={1} size="xl" />
+						</ActionIcon>
+					</Group>
+				</Center>
+			</Stack>
 		</MantineProvider>
 	);
 }
