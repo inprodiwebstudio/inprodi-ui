a134feb6af9b46037a92fa7cbfd71ab27e7a7c85
NestorNavarro <nestor.navarroarizaga@gmail.com>
2023-06-28 10:47:54 -0600

    feat(packages/inprodi-icons): interface InprodiProps exported

--- packages/inprodi-icons/src/index.ts
+++ packages/inprodi-icons/src/index.ts
@@ -4,3 +4,4 @@ export * from "./outline";
 export * as ColorIcons from "./color";
 export * as FilledIcons from "./filled";
 export * as OutlineIcons from "./outline";
+export type { IconProps } from "./Icon.interface"
